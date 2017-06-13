6bbb50779a46e1943dc5be9642be8cbb35215e72
Max Fang <maxfangx@gmail.com>
2017-06-12 19:57:49 -0700

    Complete models for Article, Payment, Profile

--- coindesk/models.py
+++ coindesk/models.py
@@ -9,24 +9,58 @@ class Profile(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
     handle = models.CharField(max_length=30)
     identity_pubkey = models.CharField(max_length=80, unique=True)
-    # lightning_address = BitcoinAddressField()
+    # bitcoin_address = BitcoinAddressField()
+
+
+class Article(models.Model):
+
+    ARTICLE_STATUS_CHOICES = (
+        ('visible', 'Visible'),
+        ('deleted by admin', 'Deleted by admin'),
+    )
+
+    title = models.CharField(max_length=191)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, default='visible', choices=ARTICLE_STATUS_CHOICES)
+
+    def views(self):
+        return self.payments.filter(status='complete', purpose='view').count()
+
+    def upvotes(self):
+        return self.payments.filter(status='complete', purpose='upvote').count()
+
+    def __str__(self):
+        return "({}) '{}'".format(self.id, self.title)
 
 
 class Payment(models.Model):
 
     PAYMENT_STATUS_CHOICES = (
-        ('pending_invoice', 'Pending Invoice'),
+        ('pending_invoice', 'Pending Invoice'), # Should be atomic
         ('pending_payment', 'Pending Payment'),
         ('complete', 'Complete'),
         ('error', 'Error'),
     )
 
-    sender = models.ForeignKey(User, related_name='senders')
-    recipient = models.ForeignKey(User, related_name='recipients')
-    amount = models.IntegerField()
-    status = models.CharField(max_length=50, default='accepted', choices=PAYMENT_STATUS_CHOICES)
+    PAYMENT_PURPOSE_CHOICES = (
+        ('view', 'View'),
+        ('upvote', 'Upvote')
+    )
+
+    user = models.ForeignKey(User)
+    article = models.ForeignKey(Article)
+    purpose = models.CharField(max_length=10, choices=PAYMENT_PURPOSE_CHOICES)
+
+    satoshi_amount = models.IntegerField()
     r_hash = models.CharField(max_length=64)
     payment_req = models.CharField(max_length=1000)
 
+    status = models.CharField(max_length=50, default='pending_invoice', choices=PAYMENT_STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
 admin.site.register(Profile)
 admin.site.register(Payment)
+admin.site.register(Article)
